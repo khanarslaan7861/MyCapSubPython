e2636087f8965ee01f8a34b37ccd01db232cf9bc
khanarslaan7861 <49835460+khanarslaan7861@users.noreply.github.com>
2022-07-24 00:09:50 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,3 @@
+import math as m
+rad = float(input("Input the radius of the circle : "))
+print(f'The area of the circle with radius {rad} is : {m.pi * rad * rad}')
---
+++ fileex.py
@@ -0,0 +1,3 @@
+fn = input("Input the Filename: ")
+fe = fn.split(".")
+print(f'The extension of the file is : {repr(fe[-1])}')
